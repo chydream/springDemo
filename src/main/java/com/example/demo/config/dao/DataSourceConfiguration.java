3c8059593ece6ca808c63dd467c44a0f6caf41a3
chenyong <yong.chen@doone.com.cn>
2019-02-27 15:34:32 +0800

    添加config配置

---
+++ src/main/java/com/example/demo/config/dao/DataSourceConfiguration.java
@@ -0,0 +1,36 @@
+package com.example.demo.config.dao;
+
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.beans.PropertyVetoException;
+
+@Configuration
+@MapperScan("com.example.demo.mapper")
+public class DataSourceConfiguration {
+    @Value("${jdbc.driver}")
+    private String jdbcDriver;
+    @Value("${jdbc.url}")
+    private String jdbcUrl;
+    @Value("${jdbc.username}")
+    private String jdbcUsername;
+    @Value("${jdbc.password}")
+    private String jdbcPassword;
+    @Bean(name="dataSource")
+    public ComboPooledDataSource createDataSource() throws PropertyVetoException {
+        ComboPooledDataSource dataSource = new ComboPooledDataSource();
+        dataSource.setDriverClass(jdbcDriver);
+        dataSource.setJdbcUrl(jdbcUrl);
+        dataSource.setUser(jdbcUsername);
+        dataSource.setPassword(jdbcPassword);
+        dataSource.setAutoCommitOnClose(false);
+        dataSource.setMaxPoolSize(30);
+        dataSource.setMinPoolSize(10);
+        dataSource.setCheckoutTimeout(10000);
+        dataSource.setAcquireRetryAttempts(2);
+        return  dataSource;
+    }
+}
---
+++ src/main/java/com/example/demo/config/dao/MyBatisConfiguration.java
@@ -0,0 +1,35 @@
+package com.example.demo.config.dao;
+
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+
+import javax.sql.DataSource;
+import java.io.IOException;
+
+@Configuration
+public class MyBatisConfiguration {
+    @Value("${mybatis_config_file}")
+    private String mybatisConfigFilePath;
+    @Value("${mapper_path}")
+    private String mapperPath;
+    @Value("${type_aliases_package}")
+    private String typeAliasesPackage;
+    @Autowired
+    private DataSource dataSource;
+    @Bean(name="sqlSessionFactory")
+    public SqlSessionFactoryBean createSqlSessionFactoryBean() throws IOException {
+        SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();
+        sqlSessionFactoryBean.setConfigLocation(new ClassPathResource(mybatisConfigFilePath));
+        PathMatchingResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
+        String packageSearchPath = PathMatchingResourcePatternResolver.CLASSPATH_ALL_URL_PREFIX+mapperPath;
+        sqlSessionFactoryBean.setMapperLocations(resolver.getResources(packageSearchPath));
+        sqlSessionFactoryBean.setDataSource(dataSource);
+        sqlSessionFactoryBean.setTypeAliasesPackage(typeAliasesPackage);
+        return  sqlSessionFactoryBean;
+    }
+}
