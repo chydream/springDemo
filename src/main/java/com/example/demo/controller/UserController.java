e6621da3f24c6eda8623358852fc103943ced4fe
chenyong <yong.chen@doone.com.cn>
2019-02-27 12:09:00 +0800

    springDemo

---
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -0,0 +1,24 @@
+package com.example.demo.controller;
+
+import com.example.demo.entity.User;
+import com.example.demo.mapper.UserMapper;
+import com.example.demo.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class UserController {
+    @Autowired
+    private UserService userService;
+    @Autowired
+    private UserMapper userMapper;
+    @RequestMapping(value="/list",method = RequestMethod.GET)
+    public List<User> list(){
+        List<User> list = userMapper.findAll();
+        return list;
+    }
+}
---
+++ src/main/java/com/example/demo/entity/User.java
@@ -0,0 +1,32 @@
+package com.example.demo.entity;
+
+public class User {
+    private Integer id;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getCupSize() {
+        return cupSize;
+    }
+
+    public void setCupSize(String cupSize) {
+        this.cupSize = cupSize;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    private String cupSize;
+    private Integer age;
+}
